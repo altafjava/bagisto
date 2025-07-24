f776c8b3888ca00978f9f3b18526dec8b44aa9e5
Altaf <samaltaf6@gmail.com>
2025-07-24 23:11:50 +0530

    add primary key in missing tables

--- packages/Webkul/CMS/src/Database/Migrations/2020_01_15_130209_create_cms_page_channels_table.php
+++ packages/Webkul/CMS/src/Database/Migrations/2020_01_15_130209_create_cms_page_channels_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->integer('cms_page_id')->unsigned();
             $table->integer('channel_id')->unsigned();
 
-            $table->unique(['cms_page_id', 'channel_id']);
+            $table->primary(['cms_page_id', 'channel_id']);
             $table->foreign('cms_page_id')->references('id')->on('cms_pages')->onDelete('cascade');
             $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
         });
